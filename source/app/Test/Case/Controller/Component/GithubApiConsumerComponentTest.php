67b654ab6e425a9eee7ea54f0eb8e579633f9a91
Chris Vogt <mail@chrisvogt.me>
2016-01-02 14:16:21 -0700

    (tests) Write unit tests for the GitHub API Consumer class.

--- source/app/Test/Case/Controller/Component/GithubApiConsumerComponentTest.php
+++ source/app/Test/Case/Controller/Component/GithubApiConsumerComponentTest.php
@@ -2,6 +2,7 @@
 App::uses('ComponentCollection', 'Controller');
 App::uses('Component', 'Controller');
 App::uses('GithubApiConsumerComponent', 'Controller/Component');
+
 /**
  * GitHub Api Consumer Component tests
  */
@@ -56,11 +57,49 @@ class GithubApiConsumerComponentTest extends CakeTestCase {
 	}
 
 /**
- * Tests correct path is assembled for GET requests to the GitHub API.
+ * Tests correct path is assembled for GET requests to the GitHub API
  */
 	public function testBuildQuery() {
 		$result = $this->GithubApiConsumer->buildQuery();
 		$this->assertEquals($result, 'https://api.github.com/repos/Joyent/node/commits?jsonp=');
+	}
+
+/**
+ * Tests `username` setter returns bool
+ */
+	public function testSetUsernameReturnsBoolean() {
+		$result = $this->GithubApiConsumer->setUsername('google');
+		$this->assertInternalType('boolean', $result);
+	}
+
+/**
+ * Tests `repository` setter returns bool
+ */
+	public function testSetRepositoryReturnsBoolean() {
+		$result = $this->GithubApiConsumer->setRepository('material-design-lite');
+		$this->assertInternalType('boolean', $result);
+	}
+
+/**
+ * Test missing `username` and `repository` throws exception
+ *
+ * @expectedException InvalidArgumentException
+ */
+	public function testGetRecentCommits() {
+		$this->GithubApiConsumer->setUsername(NULL);
+		$this->GithubApiConsumer->setRepository(NULL);
+		$result = $this->GithubApiConsumer->getRecentCommits();
+	}
+
+/**
+ * Test missing `username` and `repository` throws exception
+ *
+ * @expectedException NotFoundException
+ */
+	public function testGetRecentCommitsFromInvalidProject() {
+		$this->GithubApiConsumer->setUsername('chrisvogt');
+		$this->GithubApiConsumer->setRepository('th1sw1lln3v3r3x1st');
+		$result = $this->GithubApiConsumer->getRecentCommits();
 	}
 
 }
